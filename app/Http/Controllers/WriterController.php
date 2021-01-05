c296768df5feabc6a355918dab6e7d0e3824bdb4
Manin <maro@ozone.email>
2021-01-05 13:17:51 +0900

    Writer修正

--- app/Http/Controllers/WriterController.php
+++ app/Http/Controllers/WriterController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Writer;
 
 class WriterController extends Controller
 {
     public function index()
     {
         // DBよりWriterテーブルの値を全て取得
-        $lists = Book::all();
+        $lists = Writer::all();
 
         // 取得した値をビュー「writer/index」に渡す
         return view('writer/index', compact('lists'));
