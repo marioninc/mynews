c0f0cc827016be773409bf2d0c3281335c21de3d
Manin <maro@ozone.email>
2021-01-07 13:18:07 +0900

    APIコントローラーの変更

--- app/Http/Controllers/API/WriterController.php
+++ app/Http/Controllers/API/WriterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Writer;
 
-class WriterAPIController extends Controller
+class WriterController extends Controller
 {
     /**
      * Display a listing of the resource.
