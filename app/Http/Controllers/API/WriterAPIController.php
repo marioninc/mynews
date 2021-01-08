729ba3e27975ce560b7b7bfc7750b09f26c13502
Manin <maro@ozone.email>
2021-01-08 14:02:02 +0900

    テスト2

--- app/Http/Controllers/API/WriterAPIController.php
+++ app/Http/Controllers/API/WriterAPIController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Writer;
 
-class WriterController extends Controller
+class WriterAPIController extends Controller
 {
     /**
      * Display a listing of the resource.
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WriterController;
+use App\Http\Controllers\WriterAPIController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('writer' , WriterController::class);
+Route::apiResource('writer' , WriterAPIController::class);
